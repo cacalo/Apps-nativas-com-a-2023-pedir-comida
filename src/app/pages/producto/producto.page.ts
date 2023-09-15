cae4d3956bf3f008fa98d7681f5ec3d13e943d82
cacalo <cacalo219@hotmail.com>
2023-09-15 10:55:06 -0300

    Servicio de carrito creado Metodo Agregar a carrito Metodo Limpiar Carrito

--- src/app/pages/producto/producto.page.ts
+++ src/app/pages/producto/producto.page.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CATEGORIAS } from 'src/app/core/constants/categorias';
 import { Producto } from 'src/app/core/interfaces/productos';
+import { CarritoService } from 'src/app/core/services/carrito.service';
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.page.html',
   styleUrls: ['./producto.page.scss'],
 })
-export class ProductoPage implements OnInit {
+export class ProductoPage{
 
-  constructor(private activatedRoute: ActivatedRoute) { 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private carritoService: CarritoService) { 
     activatedRoute.params.subscribe(params => {
       CATEGORIAS.forEach(categoria => {
         const productoEncontrado = categoria.productos.find(producto => producto.id == params['id']);
@@ -21,15 +24,17 @@ export class ProductoPage implements OnInit {
     })
   }
 
-  ngOnInit() {
-  }
-
   producto:Producto = {
     id: 0,
     nombre: '',
     precio: 0,
     ingredientes: [],
     imagen: ''
+  }
+
+  onAgregarAlCarritoClicked(){
+    this.carritoService.agregarProducto(this.producto)
+    console.log(this.carritoService.carrito);
   }
 
 }
